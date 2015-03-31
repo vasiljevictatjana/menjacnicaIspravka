4235e2b12fdb3c329ddd0837f4878c34f3016a94
Tatjana <vasiljevictatjana@yahoo.com>
2015-03-31 22:37:05 +0200

    Napravljena klasa koja implementira interfejs, jos uvek prazna tela metoda

---
+++ src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+import interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	public void dodajKurs(Kurs kurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void obrisiKurs(Kurs kurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public LinkedList<Kurs> pronadjiKurs(GregorianCalendar datumKursa,
+			double prodajniKurs, double kupovniKurs, double srednjiKurs,
+			Valuta valuta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
