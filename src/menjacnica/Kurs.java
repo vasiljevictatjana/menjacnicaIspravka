ed840cf48c954b071a654e633fab0d1f04860ad6
Tatjana <vasiljevictatjana@yahoo.com>
2015-03-31 18:41:30 +0200

    Dodate metode toString, equals i hashCode u klase Valuta i Kurs

--- src/menjacnica/Kurs.java
+++ src/menjacnica/Kurs.java
@@ -31,5 +31,50 @@ public class Kurs {
 	public void setDatumKursa(GregorianCalendar datumKursa) {
 		this.datumKursa = datumKursa;
 	}
+	public String toString() {
+		return "Kurs [prodajniKurs=" + prodajniKurs + ", kupovniKurs="
+				+ kupovniKurs + ", srednjiKurs=" + srednjiKurs
+				+ ", datumKursa=" + datumKursa + "]";
+	}
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((datumKursa == null) ? 0 : datumKursa.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(prodajniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(srednjiKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datumKursa == null) {
+			if (other.datumKursa != null)
+				return false;
+		} else if (!datumKursa.equals(other.datumKursa))
+			return false;
+		if (Double.doubleToLongBits(kupovniKurs) != Double
+				.doubleToLongBits(other.kupovniKurs))
+			return false;
+		if (Double.doubleToLongBits(prodajniKurs) != Double
+				.doubleToLongBits(other.prodajniKurs))
+			return false;
+		if (Double.doubleToLongBits(srednjiKurs) != Double
+				.doubleToLongBits(other.srednjiKurs))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
--- src/menjacnica/Valuta.java
+++ src/menjacnica/Valuta.java
@@ -20,5 +20,41 @@ public class Valuta {
 		this.skraceniNaziv = skraceniNaziv;
 	}
 
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceniNaziv=" + skraceniNaziv
+				+ "]";
+	}
+
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result
+				+ ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skraceniNaziv == null) {
+			if (other.skraceniNaziv != null)
+				return false;
+		} else if (!skraceniNaziv.equals(other.skraceniNaziv))
+			return false;
+		return true;
+	}
+	
+
 	
 }
