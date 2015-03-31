a85e0ce2021dbbd2d24b6af0eb18000aaba6b815
Tatjana <vasiljevictatjana@yahoo.com>
2015-03-31 18:39:23 +0200

    Napravljene klase Kurs i Valuta sa get i set metodama

---
+++ src/menjacnica/Kurs.java
@@ -0,0 +1,35 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+public class Kurs {
+
+	private double prodajniKurs, kupovniKurs, srednjiKurs;
+	private GregorianCalendar datumKursa;
+	
+	public double getProdajniKurs() {
+		return prodajniKurs;
+	}
+	public void setProdajniKurs(double prodajniKurs) {
+		this.prodajniKurs = prodajniKurs;
+	}
+	public double getKupovniKurs() {
+		return kupovniKurs;
+	}
+	public void setKupovniKurs(double kupovniKurs) {
+		this.kupovniKurs = kupovniKurs;
+	}
+	public double getSrednjiKurs() {
+		return srednjiKurs;
+	}
+	public void setSrednjiKurs(double srednjiKurs) {
+		this.srednjiKurs = srednjiKurs;
+	}
+	public GregorianCalendar getDatumKursa() {
+		return datumKursa;
+	}
+	public void setDatumKursa(GregorianCalendar datumKursa) {
+		this.datumKursa = datumKursa;
+	}
+	
+}
---
+++ src/menjacnica/Valuta.java
@@ -0,0 +1,24 @@
+package menjacnica;
+
+public class Valuta {
+
+	private String naziv, skraceniNaziv;
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+
+	
+}
