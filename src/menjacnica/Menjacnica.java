f2a0e14145e340c4d12c8fa5c2bdf5dbbb04e83d
Tatjana <vasiljevictatjana@yahoo.com>
2015-03-31 22:40:22 +0200

    Implemntirana klasa Menjacnica

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -5,23 +5,53 @@ import java.util.LinkedList;
 
 import interfejs.MenjacnicaInterfejs;
 
+
 public class Menjacnica implements MenjacnicaInterfejs {
+	
+	private LinkedList<Kurs> kursevi = 
+			new LinkedList<Kurs>();
 
 	public void dodajKurs(Kurs kurs) {
-		// TODO Auto-generated method stub
+		
+		if(kurs.getDatumKursa().before(new GregorianCalendar(GregorianCalendar.YEAR, GregorianCalendar.MONTH, GregorianCalendar.DAY_OF_MONTH)))
+			throw new RuntimeException("Datum ne sme biti u proslosti!");
+	
+
+		if (kursevi.contains(kurs))
+			throw new RuntimeException("Kurs vec postoji!");
+		
+		kursevi.add(kurs);
+
 
 	}
 
 	public void obrisiKurs(Kurs kurs) {
-		// TODO Auto-generated method stub
-
+		
+		if (!kursevi.contains(kurs))
+			throw new RuntimeException("Kurs ne postoji!");
+		
+		kursevi.remove(kurs);
+		
 	}
 
 	public LinkedList<Kurs> pronadjiKurs(GregorianCalendar datumKursa,
 			double prodajniKurs, double kupovniKurs, double srednjiKurs,
 			Valuta valuta) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		LinkedList<Kurs> pronadjeniKursevi = 
+				new LinkedList<Kurs>();
+		
+		if (datumKursa==null && prodajniKurs<=0 && kupovniKurs<=0 && srednjiKurs<=0)
+			throw new RuntimeException("Mora biti unet neki kriterijum!");
+		
+		if (!datumKursa.after(new GregorianCalendar(GregorianCalendar.YEAR, GregorianCalendar.MONTH, GregorianCalendar.DAY_OF_MONTH)))
+			for (int i=0;i<kursevi.size();i++)
+				if (kursevi.get(i).getDatumKursa()==datumKursa)
+					pronadjeniKursevi.add(kursevi.get(i));
+		
+
+		
+		return pronadjeniKursevi;
 	}
 
 }
